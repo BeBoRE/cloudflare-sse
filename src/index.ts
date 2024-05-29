b0df9f6e4bbb1f8515e250264709eb18a132417f
Ayrton-Taede Tromp <ayrton@bebore.com>
2024-05-29 16:57:41 +0200

    SSE example added

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,53 @@
 /**
  * Welcome to Cloudflare Workers! This is your first worker.
  *
- * - Run `npm run dev` in your terminal to start a development server
+ * - Run "npm run dev" in your terminal to start a development server
  * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
+ * - Run "npm run deploy" to publish your worker
  *
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+async function writeToStream(writable : WritableStream) {
+
+  const writer = writable.getWriter();
+  
+  let count = 0;
+  let eventId = `id-${count}`;
+  let msgBody = `{status: ${true}, text: "Hello to SSE message", time: "${new Date().toISOString()}"}`;
+
+  // send hello message
+  await constructSSE(writer, eventId, "userConnected", msgBody);
+
+  // send message every 5 second
+  setInterval(function () {
+		count++;
+    eventId = `id-${count}`;
+    msgBody = `{status: ${true}, text: "Repeat message: ${count}", time: "${new Date().toISOString()}"}`;
+    constructSSE(writer, eventId, "userMessage", msgBody);
+
+		console.log(`Sent message: ${count}`);
+  }, 1000);
+}
+
+async function constructSSE(writer : WritableStreamDefaultWriter<any>, eventId : string, eventType : string, msgBody : string) {
+  const encoder = new TextEncoder();
+
+  await writer.write(encoder.encode(`id: ${eventId}` + '\n'));
+  if (eventType) {
+    await writer.write(encoder.encode(`event: ${eventType}` + '\n'));
+  }
+  await writer.write(encoder.encode(`data: ${msgBody}`+ '\n\n'));
+}
+
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
-	},
-};
+	async fetch() {
+		const { readable, writable } = new TransformStream()
+		let headers = new Headers();
+		headers.append('Content-Type', 'text/event-stream');
+		const init = { "status": 200, "statusText": "ok", "headers": headers };
+	
+		writeToStream(writable);
+		return new Response(readable, init);
+	}
+}
